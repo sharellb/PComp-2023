8981f659c7e07893146e587690c3d1ddf91c395a
Sharell Bryant <sharell.bryant@gmail.com>
2023-10-30 21:35:37 -0400

    Different formats

--- Week-7/intro-to-serial/intro-to-serial.ino
+++ Week-7/intro-to-serial/intro-to-serial.ino
@@ -4,7 +4,21 @@ void setup() {
 }
  
 void loop() {
-   int analogValue = analogRead(A0);
-   int mappedValue = map(analogValue, 0, 1023, 0, 255);
-   Serial.println(mappedValue);
+// read analog input, map it to make the range 0-255:
+int analogValue = analogRead(A0);
+int mappedValue = map(analogValue, 0, 1023, 0, 255);
+Serial.println(mappedValue);
+ 
+// print different formats:
+Serial.write(mappedValue);  // Print the raw binary value
+Serial.print('\t');             // print a tab
+// print ASCII-encoded values:
+Serial.print(mappedValue, BIN); // print ASCII-encoded binary value
+Serial.print('\t');             // print a tab
+Serial.print(mappedValue);      // print decimal value
+Serial.print('\t');             // print a tab
+Serial.print(mappedValue, HEX); // print hexadecimal value
+Serial.print('\t');             // print a tab
+Serial.print(mappedValue, OCT); // print octal value
+Serial.println();               // print linefeed and carriage return
 }
