f05a5d64d1d801e6ef354bee01bcad82cdd03a6e
Sharell Bryant <sharell.bryant@gmail.com>
2023-10-30 21:49:28 -0400

    call and response

--- Week-7/intro-to-serial/intro-to-serial.ino
+++ Week-7/intro-to-serial/intro-to-serial.ino
@@ -1,27 +1,33 @@
 const int switchPin = 2;      // digital input
+int sensorValue;
  
 void setup() {
-   // configure the serial connection:
    Serial.begin(9600);
-   // configure the digital input:
-   pinMode(switchPin, INPUT);
+   while (Serial.available() <= 0) {
+     Serial.println("hello"); // send a starting message
+     delay(300);              // wait 1/3 second
+   }
 }
 
 void loop() {
-   // read the sensor:
-   int sensorValue = analogRead(A0);
-   // print the results:
-   Serial.print(sensorValue);
-   Serial.print(",");
+   if (Serial.available()) {
+      // read the incoming byte:
+      int inByte = Serial.read();
+      // read the sensor:
+      sensorValue = analogRead(A0);
+      // print the results:
+      Serial.print(sensorValue);
+      Serial.print(",");
  
-   // read the sensor:
-   sensorValue = analogRead(A1);
-   // print the results:
-   Serial.print(sensorValue);
-   Serial.print(",");
+      // read the sensor:
+      sensorValue = analogRead(A1);
+      // print the results:
+      Serial.print(sensorValue);
+      Serial.print(",");
  
-   // read the sensor:
-   sensorValue = digitalRead(switchPin);
-   // print the results:
-   Serial.println(sensorValue);
+      // read the sensor:
+      sensorValue = digitalRead(switchPin);
+      // print the results:
+      Serial.println(sensorValue);
+   }
 }
